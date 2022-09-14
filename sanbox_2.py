547b31b350f8bee378a709f2d4b04b60b77b4d77
sarobinson2001 <sarobinson2011@gmail.com>
2022-09-14 19:06:41 +0100

    added linked_lists.py for learning
    added sandbox_2.py:
    which here has the bare bones for leetcode #20 - romanToInteger

--- sanbox_2.py
+++ sanbox_2.py
@@ -3,3 +3,8 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         # Map the Roman numerals to their corresponding decimal value
         mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        print(mapping)
+
+
+sol = Solution()
+sol.intToRoman(2)

15fb54bf3257dfbcad9ffc120b1d9c94a0b354db
sarobinson2011 <sarobinson2011@gmail.com>
2022-09-23 10:41:35 +0100

    1) updated sandbox.py to contain my 'linked list' code to actually build out (and simulate) the example from leetcode #21 (merge two linked lists)
    2) renamed sanbox_2.py --> sandbox_2.py

--- sandbox.py
+++ sandbox.py
@@ -9,30 +9,14 @@ class Linkedlist:
     def __init__(self):
         self.head_val = None
 
-    def build_lists(self):
-        list1 = Linkedlist()
-        list2 = Linkedlist()
-        list1.head_val = Node('1')
-        list2.head_val = Node('1')
-        list1_2 = Node('3')
-        list2_2 = Node('5')
-        list1_3 = Node('4')
-        list2_3 = Node('6')
-        # connect 1st node to 2nd node
-        list1.head_val.next = list1_2
-        list2.head_val.next = list2_2
-        # connect 2nd node to 3rd node
-        list1_2.next = list1_3
-        list2_2.next = list2_3
-
     def print_lists(self):
         out = []
         print_val = self.head_val
         while print_val is not None:
             out.append(print_val.val)
             print_val = print_val.next
-        for count in range(1):
-            print(f'list {count} =',out)
+        # for i in range(1):
+        print(f'list =',out)
 
     def merge_two_lists(self, list1: [Node], list2: [Node]) -> [Node]:
         """ first declare a dummy node to act as the head of the list """
@@ -42,19 +26,19 @@ class Linkedlist:
         # while both lists are non-empty
         while list1 and list2:
             if list1.val <= list2.val:
-                merged_list.next = list1
+                cur.next = list1
                 list1 = list1.next
             else:
-                merged_list = list2.val
+                cur.next = list2
                 list2 = list2.next
 
         if list2:       # if list1 is empty
-            merged_list.next = list2
+            cur.next = list2
         elif list1:     # elif list 2 is empty
-            merged_list.next = list1
+            cur.next = list1
 
-        print(merged_list.next)
-        # return merged_list.next
+        # print(merged_list.next)
+        return merged_list.next
 
 
 # create 3 instances of linked lists
@@ -78,6 +62,9 @@ list2_2.next = list2_3
 # run the print_lists function on the 2 lists
 my_list1.print_lists()
 my_list2.print_lists()
+
+output_list.merge_two_lists(my_list1.head_val, my_list2.head_val)
+output_list.print_lists()
 
 
 

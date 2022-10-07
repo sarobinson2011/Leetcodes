e2b2b8ccf2a5295661d95cbf29cb2ca49dc31a5f
sarobinson2001 <sarobinson2011@gmail.com>
2022-10-07 15:17:29 +0100

    leetcode_58_lengthOfLastWord added

--- sandbox.py
+++ sandbox.py
@@ -1,29 +1,27 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+""" leetcode_58 modified to return the length of the longest word """
+
 
 class Solution:
-    def mergeTwoLists(self, list1: [ListNode], list2: [ListNode]) -> [ListNode]:
-        """ merge lists """
-        merged_list = [ListNode]
 
-        merged_list.val = list1.val
+    def lengthOfLongestWord(self, s: str) -> int:
+        count = 0
+        store = []
+        # for c in s[::-1]:
+        for c in s[::-1]:
 
-        while list2.next != None
-            if list1.next > list2.val:
-                merged_list.next = list2.val
-            else:
-                merged_list.next = list1.val
+            if c != "-":
+                count += 1
+            elif count > 0:
+                store.append(count)
+                count = 0
 
-
-
-
-
-
-
+        store.append(count)
+        print(max(store))
+        return max(store)
 
 
-
+sol = Solution()
+sol.lengthOfLongestWord("a")               # return 1
+sol.lengthOfLongestWord("-a")              # return 1
+sol.lengthOfLongestWord("meme--11111123--")     # return 4
 

f1e8b1ee89dce1e0763c543658837ac2bc90b00b
sarobinson2011 <sarobinson2011@gmail.com>
2022-10-26 19:49:20 +0100

    added solution to leetcode_69_sqrt(x) + draft leetcode_70_climbingStairs

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,3 @@
+""" working solution to leetcode_70_climbingStairs """
 class Solution:
-    def fibonacci(self, limit: int):
-        out = [0, 1]
-        for i in range(limit):
-            temp = out[i] - out[i-1]
-            out.append(temp)
-            print('out =', out)
-
-
-sol = Solution()
-sol.fibonacci(5)
-
-""" recursively calculate Fibonacci numbers (to the limit) """
+    def climbStairs(self, n: int) -> int:

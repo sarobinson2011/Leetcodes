4ef1d0d4177a7e11230c2ca53efed917b2789c13
sarobinson2011 <sarobinson2011@gmail.com>
2022-10-27 14:48:41 +0100

    added solution to leetcode_70_climbingStairs - using iteration + stack to solve the recursive Fibonacci sequence problem

--- sandbox.py
+++ sandbox.py
@@ -1,24 +1,9 @@
-""" working solution to leetcode_70_climbingStairs """
+""" draft solution to leetcode_83_deleteDuplicates  """
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-
-    def climbStairs(self, n: int) -> int:
-        res = [1, 1]
-        for i in range(1, n):
-            temp = res[-1] + res[-2]
-            res.append(temp)
-        print(res[-1])
-        return res[-1]
-
-            # with open('output.txt', 'w') as f:
-            #     print(f'\nFibonacci numbers: {res}', file=f)
-
-
-sol = Solution()
-sol.climbStairs(1)
-sol.climbStairs(2)
-sol.climbStairs(3)
-sol.climbStairs(4)
-sol.climbStairs(5)
-sol.climbStairs(6)
-sol.climbStairs(7)
-#  1, 1, 2, 3, 5, 8, 13, 21
+    def deleteDuplicates(self, head: [ListNode]) -> [ListNode]:
